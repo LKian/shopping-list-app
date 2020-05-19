d3199bbca9f6ac63a67aecbf90284f74b2d46c3d
Leah <LeahKahwadjian@gmail.com>
2020-05-18 23:37:24 -0400

    added client side, material ui, data for static page, started building components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Posts from "./Posts";
+import "../App.css";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h2>Front and back end run!</h2>
+      <Posts />
     </div>
   );
 }
