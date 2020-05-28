013c0c1b194bad0657bd6252315a7410178aa75a
Leah <LeahKahwadjian@gmail.com>
2020-05-27 21:11:40 -0400

    fixed delete request on back end

--- client/src/components/GroceryList.js
+++ client/src/components/GroceryList.js
@@ -14,7 +14,6 @@ var axios = require("axios");
 class GroceryList extends Component {
   state = {
     groceryItemDB: "",
-
     groceryListDB: [],
   };
 
@@ -22,7 +21,10 @@ class GroceryList extends Component {
     return axios
       .get("http://localhost:5000/groceries/")
       .then((res) => {
-        console.log("this is getallItems RES: ", res.data);
+        console.log(
+          "Grocery List: ",
+          res.data.map((groceryItem) => groceryItem.name)
+        );
         const groceryListDB = res.data;
 
         this.setState({ groceryListDB: groceryListDB });
@@ -44,59 +46,59 @@ class GroceryList extends Component {
     };
     axios
       .post("http://localhost:5000/groceries", groceryItemDB)
+
       .then((res) => {
-        this.setState((prevState) => ({
-          groceryItemDB: "",
-          groceryListDB: [...this.state.groceryListDB, res.data],
-        }));
+        if (res.status === 200) {
+          console.log("You added: ", res.data.name);
+          this.setState((prevState) => ({
+            groceryItemDB: "",
+            groceryListDB: [...this.state.groceryListDB, res.data],
+          }));
+        }
       })
       .catch((e) =>
         this.setState({ error: "People who use grocery lists are no fun" })
       );
   };
 
-  deleteItem = (e, id) => {
-    this.setState((prevState) => ({
-      groceryListDB: this.state.groceryListDB.filter(
-        (currentGroceryItem) => currentGroceryItem._id !== id
-      ),
-    }));
-    console.log(`delete second part: http://localhost:5000/groceries/${id}`);
+  deleteItem = (id) => {
     axios.delete(`http://localhost:5000/groceries/${id}`).then((res) => {
-      console.log(
-        "this is state ",
-        this.state.groceryListDB,
-        "this is id ",
-        id
-      );
+      if (res.status === 200) {
+        console.log("You deleted: ", id, res);
+        this.setState((prevState) => ({
+          groceryListDB: this.state.groceryListDB.filter(
+            (currentGroceryItem) => currentGroceryItem._id !== id
+          ),
+        }));
+      }
     });
   };
 
   render() {
     const { groceryList, groceryListDB } = this.state;
 
-    const databaseItems = groceryListDB.map((databaseItem, key) => (
+    const databaseItems = groceryListDB.map((databaseItem, index) => (
       <List className="list-condensed">
-        <ListItem key={databaseItem._id}>
+        <ListItem key={index.toString()}>
           <ListItemIcon className="icon-container">
             <DeleteIcon
               className="icon-delete"
-              id={databaseItem.id}
-              onClick={(e) => this.deleteItem(e, databaseItem._id)}
+              id={databaseItem._id}
+              onClick={(e) => this.deleteItem(databaseItem._id)}
             />
           </ListItemIcon>
           <ListItemText primary={databaseItem.name} />
-          {/* <ListItemText primary={databaseItem._id} /> */}
         </ListItem>
       </List>
     ));
 
     return (
       <Container maxWidth="lg" className="section">
+        {this.state.error}
         <form id="grocery-list-db-form" onSubmit={this.addItemToDB}>
           <TextField
-            id="outlined-basic-db"
-            label="Add a grocery item to the database"
+            id="grocery-item-name"
+            label="Add a grocery item"
             value={this.state.groceryItemDB}
             onChange={this.handleChangeDB}
             variant="outlined"
